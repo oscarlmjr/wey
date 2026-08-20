18d72ead61547e11cbdeecb7b7efbd61ebacfada
oscarlmjr <oscarlmjr@gmail.com>
2026-08-20 13:10:57 -0300

    1_Hello App.vue 10min

--- wey_frontend/src/main.js
+++ wey_frontend/src/main.js
@@ -5,10 +5,10 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+
 import axios from 'axios'
 
 axios.defaults.baseURL = 'http://127.0.0.1:8000'
-
 const app = createApp(App)
 
 app.use(createPinia())
