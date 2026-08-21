d8e21e3eb516115ab360f6885974977e8e5b4686
oscarlmjr <oscarlmjr@gmail.com>
2026-08-21 15:00:50 -0300

    part_1 4_Create static messages page

--- wey_frontend/src/router/index.js
+++ wey_frontend/src/router/index.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import SignupView from '../views/SignupView.vue'
 import LoginView from '../views/LoginView.vue'
+import FeedView from '../views/FeedView.vue'
+import MessagesView from '../views/MessagesView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,6 +14,11 @@ const router = createRouter({
       component: HomeView,
     },
     {
+      path: '/feed',
+      name: 'feed',
+      component: FeedView
+    },
+    {
       path: '/signup',
       name: 'signup',
       component: SignupView
@@ -20,6 +27,11 @@ const router = createRouter({
       path: '/login',
       name: 'login',
       component: LoginView
+    },
+    {
+      path: '/messages',
+      name: 'messages',
+      component: MessagesView
     },
     {
       path: '/about',
