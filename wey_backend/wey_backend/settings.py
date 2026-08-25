7532eeb37cc9c7911f7c9418db2aadefd7967fa5
oscarlmjr <oscarlmjr@gmail.com>
2026-08-25 11:22:52 -0300

    Part_2

--- wey_backend/wey_backend/settings.py
+++ wey_backend/wey_backend/settings.py
@@ -19,7 +19,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-# AUTH_USER_MODEL = 'account.User'
+AUTH_USER_MODEL = 'account.User'
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=30),
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'account',
     'rest_framework',
     'rest_framework_simplejwt',
     'corsheaders',
