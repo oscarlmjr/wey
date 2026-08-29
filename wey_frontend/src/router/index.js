8e45ee86c80a49a875f50b545024db097aeb9c75
oscarlmjr <oscarlmjr@gmail.com>
2026-08-29 16:20:24 -0300

    Part_4 12_Fix correct name on the profile

--- wey_frontend/src/router/index.js
+++ wey_frontend/src/router/index.js
@@ -1,10 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import FeedView from '../views/FeedView.vue'
 import SignupView from '../views/SignupView.vue'
 import LoginView from '../views/LoginView.vue'
-import FeedView from '../views/FeedView.vue'
 import MessagesView from '../views/MessagesView.vue'
 import SearchView from '../views/SearchView.vue'
+import ProfileView from '../views/ProfileView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,7 +13,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView,
+      component: HomeView
     },
     {
       path: '/feed',
@@ -40,14 +41,19 @@ const router = createRouter({
       component: SearchView
     },
     {
+      path: '/profile/:id',
+      name: 'profile',
+      component: ProfileView
+    },
+    {
       path: '/about',
       name: 'about',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
+      component: () => import('../views/AboutView.vue')
+    }
+  ]
 })
 
 export default router
--- wey_frontend/src/views/ProfileView.vue
+++ wey_frontend/src/views/ProfileView.vue
@@ -1,6 +1,19 @@
 <template>
     <div class="max-w-7xl mx-auto grid grid-cols-4 gap-4">
-        <div class="main-center col-span-3 space-y-4">
+        <div class="main-left col-span-1">
+            <div class="p-4 bg-white border border-gray-200 text-center rounded-lg">
+                <img src="https://i.pravatar.cc/300?img=10" class="mb-6 rounded-full">
+                
+                <p><strong>{{ user.name }}</strong></p>
+
+                <div class="mt-6 flex space-x-8 justify-around">
+                    <p class="text-xs text-gray-500">182 friends</p>
+                    <p class="text-xs text-gray-500">120 posts</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="main-center col-span-2 space-y-4">
             <div class="bg-white border border-gray-200 rounded-lg">
                 <form v-on:submit.prevent="submitForm" method="post">
                     <div class="p-4">  
@@ -72,9 +85,18 @@
 import axios from 'axios'
 import PeopleYouMayKnow from '../components/PeopleYouMayKnow.vue'
 import Trends from '../components/Trends.vue'
+import { useUserStore } from '@/stores/user'
 
 export default {
     name: 'FeedView',
+
+    setup() {
+        const userStore = useUserStore()
+
+        return {
+            userStore
+        }
+    },
 
     components: {
         PeopleYouMayKnow,
@@ -84,6 +106,7 @@ export default {
     data() {
         return {
             posts: [],
+            user: {},
             body: '',
         }
     },
@@ -91,15 +114,20 @@ export default {
     mounted() {
         this.getFeed()
     },
+    
+    updated() {
+        this.getFeed()
+    },
 
     methods: {
         getFeed() {
             axios
-                .get('/api/posts/')
+                .get(`/api/posts/profile/${this.$route.params.id}/`)
                 .then(response => {
                     console.log('data', response.data)
 
-                    this.posts = response.data
+                    this.posts = response.data.posts
+                    this.user = response.data.user
                 })
                 .catch(error => {
                     console.log('error', error)
