369fc3de3eff7d77b215d1dec2d835ea99479796
oscarlmjr <oscarlmjr@gmail.com>
2026-09-01 17:58:51 -0300

    Part_7 19_Make it possible to discuss a post

--- wey_frontend/src/views/PostView.vue
+++ wey_frontend/src/views/PostView.vue
@@ -1,26 +1,31 @@
 <template>
     <div class="max-w-7xl mx-auto grid grid-cols-4 gap-4">
         <div class="main-center col-span-3 space-y-4">
+            <div 
+                class="p-4 bg-white border border-gray-200 rounded-lg"
+                v-if="post.id"
+            >
+                <FeedItem v-bind:post="post" />
+            </div>
+
+            <div
+                class="p-4 ml-6 bg-white border border-gray-200 rounded-lg"
+                v-for="comment in post.comments"
+                v-bind:key="comment.id"
+            >
+                <CommentItem v-bind:comment="comment" />
+            </div>
+
             <div class="bg-white border border-gray-200 rounded-lg">
                 <form v-on:submit.prevent="submitForm" method="post">
                     <div class="p-4">  
-                        <textarea v-model="body" class="p-4 w-full bg-gray-100 rounded-lg" placeholder="What are you thinking about?"></textarea>
+                        <textarea v-model="body" class="p-4 w-full bg-gray-100 rounded-lg" placeholder="What do you think?"></textarea>
                     </div>
 
-                    <div class="p-4 border-t border-gray-100 flex justify-between">
-                        <a href="#" class="inline-block py-4 px-6 bg-gray-600 text-white rounded-lg">Attach image</a>
-
-                        <button class="inline-block py-4 px-6 bg-purple-600 text-white rounded-lg">Post</button>
+                    <div class="p-4 border-t border-gray-100">
+                        <button class="inline-block py-4 px-6 bg-purple-600 text-white rounded-lg">Comment</button>
                     </div>
                 </form>
-            </div>
-
-            <div 
-                class="p-4 bg-white border border-gray-200 rounded-lg"
-                v-for="post in posts"
-                v-bind:key="post.id"
-            >
-                <FeedItem v-bind:post="post" />
             </div>
         </div>
 
@@ -37,35 +42,40 @@ import axios from 'axios'
 import PeopleYouMayKnow from '../components/PeopleYouMayKnow.vue'
 import Trends from '../components/Trends.vue'
 import FeedItem from '../components/FeedItem.vue'
+import CommentItem from '../components/CommentItem.vue'
 
 export default (await import('vue')).defineComponent ({
-    name: 'FeedView',
+    name: 'PostView',
 
     components: {
         PeopleYouMayKnow,
         Trends,
-        FeedItem
+        FeedItem,
+        CommentItem
     },
 
     data() {
         return {
-            posts: [],
-            body: '',
+            post: {
+                id: null,
+                comments: []
+              },
+            body: ''
         }
     },
 
     mounted() {
-        this.getFeed()
+        this.getPost()
     },
 
     methods: {
-        getFeed() {
+        getPost() {
             axios
-                .get('/api/posts/')
-                .then(response => {
+            .get(`/api/posts/${this.$route.params.id}/`)
+            .then(response => {
                     console.log('data', response.data)
 
-                    this.posts = response.data
+                    this.posts = response.data.post
                 })
                 .catch(error => {
                     console.log('error', error)
@@ -76,13 +86,14 @@ export default (await import('vue')).defineComponent ({
             console.log('submitForm', this.body)
 
             axios
-                .post('/api/posts/create/', {
+                .post(`/api/posts/${this.$route.params.id}/comment/`, {
                     'body': this.body
                 })
                 .then(response => {
                     console.log('data', response.data)
 
-                    this.posts.unshift(response.data)
+                    this.post.comments.push(response.data)
+                    this.post.comments_count += 1
                     this.body = ''
                 })
                 .catch(error => {
