80e4c35f0b5180a6e88b8f5d003affc60c8e241a
oscarlmjr <oscarlmjr@gmail.com>
2026-09-01 12:15:50 -0300

    Part_6 18_Make it possible to like a post

--- wey_frontend/src/views/SignupView.vue
+++ wey_frontend/src/views/SignupView.vue
@@ -103,7 +103,6 @@ export default {
                 axios
                     .post('/api/signup/', this.form)
                     .then(response => {
-                        console.log('response.data.message => ', response.data.message);
                         if (response.data.message === 'success') {
                             this.toastStore.showToast(5000, 'The user is registered. Please log in', 'bg-emerald-500')
 
